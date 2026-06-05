c278b6f39a00e3b8ae72f14ff321b4d491ee1919
Morargas <139104004+Morargas@users.noreply.github.com>
2026-06-05 23:11:05 +0700

    )

--- firebase.config.ts
+++ firebase.config.ts
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAI8SNpZJ4qS4qT4mqWbhv3lrm3Bqitbkg",
-  authDomain: "restaurant-app-b1a22.firebaseapp.com",
-  projectId: "restaurant-app-b1a22",
-  storageBucket: "restaurant-app-b1a22.firebasestorage.app",
-  messagingSenderId: "1032550766707",
-  appId: "1:1032550766707:web:602fd60a012dde5e59b206"
+
 };
 
 const app = initializeApp(firebaseConfig);
